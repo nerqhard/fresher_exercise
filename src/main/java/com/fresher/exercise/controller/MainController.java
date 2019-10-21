94698d9fdae21bfbee21fce9920a1af1f75c2407
QuangHa <quangha12thxd@gmail.com>
2019-10-21 15:51:12 +0700

    Connect Database User Details 1

--- src/main/java/com/fresher/exercise/controller/MainController.java
+++ src/main/java/com/fresher/exercise/controller/MainController.java
@@ -1,83 +1,82 @@
-package com.fresher.exercise.controller;
-
-import com.fresher.exercise.entity.User;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Controller
-public class MainController {
-
-    // use List instead of Database
-    List<User> userList = new ArrayList<>();
-
-    @GetMapping(value = {"/listUser", "/"})
-    public String list(Model model) {
-        model.addAttribute("userList", userList);
-        // return Template listUser.html
-        return "listUser";
-    }
-
-    @GetMapping("/addUser")
-    public String add(Model model) {
-        // add user to  model
-        model.addAttribute("user", new User());
-        // return Template addUser.html
-        return "addUser";
-    }
-
-    @PostMapping("/addUser")
-    public String add(User user) {
-        // add user to  userList
-        user.setId(userList.size() + 1);
-        userList.add(user);
-        return "redirect:/listUser";
-    }
-
-    @GetMapping("/editUser/{id}")
-    public String edit(@PathVariable("id") int id, Model model) {
-        for (User user : userList) {
-            if (user.getId() == id) {
-                model.addAttribute("user", user);
-                break;
-            }
-        }
-        return "/editUser";
-    }
-
-    @PostMapping("/editUser")
-    public String update(User user) {
-
-        for (User u : userList) {
-            if (u.getId() == user.getId()) {
-                u.setName(user.getName());
-                u.setAge(user.getAge());
-            }
-        }
-
-        return "redirect:/listUser";
-    }
-
-    @GetMapping("/delete/{id}")
-    public String deleteUser(@PathVariable("id") int id, Model model) {
-        if (id == 0 || String.valueOf(id).isEmpty()) {
-            model.addAttribute("nullId", "nullId");
-            return "redirect:/listUser";
-        }
-
-        for (int i = 0; i < userList.size(); i++) {
-            if (userList.get(i).getId() == id) {
-                userList.remove(userList.get(i));
-            }
-        }
-
-        return "redirect:/listUser";
-    }
-
-}
+//package com.fresher.exercise.controller;
+//
+//import com.fresher.exercise.entity.User;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@Controller
+//public class MainController {
+//
+//    // use List instead of Database
+//    List<User> userList = new ArrayList<>();
+//
+//    @GetMapping(value = {"/listUser", "/"})
+//    public String list(Model model) {
+//        model.addAttribute("userList", userList);
+//        // return Template listUser.html
+//        return "listUser";
+//    }
+//
+//    @GetMapping("/addUser")
+//    public String add(Model model) {
+//        // add user to  model
+//        model.addAttribute("user", new User());
+//        // return Template addUser.html
+//        return "addUser";
+//    }
+//
+//    @PostMapping("/addUser")
+//    public String add(User user) {
+//        // add user to  userList
+//        user.setId(userList.size() + 1);
+//        userList.add(user);
+//        return "redirect:/listUser";
+//    }
+//
+//    @GetMapping("/editUser/{id}")
+//    public String edit(@PathVariable("id") int id, Model model) {
+//        for (User user : userList) {
+//            if (user.getId() == id) {
+//                model.addAttribute("user", user);
+//                break;
+//            }
+//        }
+//        return "/editUser";
+//    }
+//
+//    @PostMapping("/editUser")
+//    public String update(User user) {
+//
+//        for (User u : userList) {
+//            if (u.getId() == user.getId()) {
+//                u.setName(user.getName());
+//                u.setAge(user.getAge());
+//            }
+//        }
+//
+//        return "redirect:/listUser";
+//    }
+//
+//    @GetMapping("/delete/{id}")
+//    public String deleteUser(@PathVariable("id") int id, Model model) {
+//        if (id == 0 || String.valueOf(id).isEmpty()) {
+//            model.addAttribute("nullId", "nullId");
+//            return "redirect:/listUser";
+//        }
+//
+//        for (int i = 0; i < userList.size(); i++) {
+//            if (userList.get(i).getId() == id) {
+//                userList.remove(userList.get(i));
+//            }
+//        }
+//
+//        return "redirect:/listUser";
+//    }
+//
+//}
--- src/main/java/com/fresher/exercise/entity/User.java
+++ src/main/java/com/fresher/exercise/entity/User.java
@@ -4,15 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "user")
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
     private int id;
 
+    @Column(name = "name_user")
     private String name;
 
+//    @Pattern(regexp = "[0-9]")
+    @Column(name = "age_user")
     private int age;
 
 }
